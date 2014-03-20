ac58c4cd8cfc52ba2c24fd76dfe5a746f1dfaf9e
Joey <admin@OSX109.local>
2014-03-20 15:10:26 +0100

    Initial Commit

---
+++ IOS_ToDoTests/IOS_ToDoTests.m
@@ -0,0 +1,34 @@
+//
+//  IOS_ToDoTests.m
+//  IOS_ToDoTests
+//
+//  Created by Joey on 20-03-14.
+//  Copyright (c) 2014 Frank & Joey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IOS_ToDoTests : XCTestCase
+
+@end
+
+@implementation IOS_ToDoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
